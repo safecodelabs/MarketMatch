9353d79ad157d95bf1a1f095740b1d237ae96bbb
tiwxri <tiwxri@gmail.com>
2025-05-28 17:18:21 +0530

    Added the webhook logic for the Meta validation

---
+++ src/routes/webHook.js
@@ -0,0 +1,28 @@
+app.get('/webhook', (req, res) => {
+  const VERIFY_TOKEN = "my_verify_token";
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token && mode === 'subscribe' && token === VERIFY_TOKEN) {
+    console.log('WEBHOOK_VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+// Handle incoming messages
+app.post('/webhook', (req, res) => {
+  const body = req.body;
+
+  if (body.object === 'whatsapp_business_account') {
+    const message = body.entry[0].changes[0].value.messages[0];
+    console.log("Received message: ", message);
+    
+    // TODO: Respond using the Send Message API
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+});
