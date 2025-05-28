5677d69064e85ebac58be0c227eb97173414b2e7
tiwxri <tiwxri@gmail.com>
2025-05-28 17:28:08 +0530

    Added the GET logic to server.js for Webhook Verification

--- server.js
+++ server.js
@@ -3,17 +3,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const adRoutes = require('./src/features/ads/ads.routes');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/api', adRoutes); // Now available at /api/ad-info
 
-app.listen(3000, () => {
-  console.log('Bot API running on http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Bot API running on http://localhost:${PORT}`);
 });
 
 const { onMessageReceived } = require('./src/bots/whatsappBot');
+
+app.get('/webhook', (req, res) => {
+  const VERIFY_TOKEN = "marketmatchai";
+
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    console.log('WEBHOOK_VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
 
 app.post('/webhook', async (req, res) => {
   const userMessage = req.body.message;
