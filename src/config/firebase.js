71c052d3ae1d9c7a1e042da36d8caad2c6de34e0
tiwxri <tiwxri@gmail.com>
2025-05-31 20:39:10 +0530

    updated the logic in firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,11 @@
 const admin = require('firebase-admin');
-const { getFirestore } = require('firebase-admin/firestore');
-
-const serviceAccount = require('../../creds/firebase-service-account.json'); // adjust path
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
-const db = getFirestore();
-
-module.exports = { db };
+module.exports = admin;
